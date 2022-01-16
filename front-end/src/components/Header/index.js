b7fe764419ee2d693276f614b5f74d68940a8821
Bwogi Andrew <andrew.bwogi@gmail.com>
2022-01-15 20:22:17 -0800

    added products to front end

--- front-end/src/components/Header/index.js
+++ front-end/src/components/Header/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import Logo from '../Logo';
 
 const Header = () => {
-	return <header>Header</header>;
+	return (
+		<header>
+			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+				<Container>
+					<Navbar.Brand href='/'>
+						<i
+							style={{ color: 'orange', fontSize: '1em' }}
+							className='fa-brands fa-slideshare'></i>
+						{/* <Logo /> */}
+						<i
+							style={{ color: 'yellow', paddingRight: 10 }}
+							class='fa-solid fa-leaf'></i>
+						My Online Shop
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav style={{ marginLeft: 'auto' }}>
+							<Nav.Link href='/cart'>
+								<i className='fa fa-shopping-cart'></i> Cart
+							</Nav.Link>
+							<Nav.Link href='/login'>
+								<i className='fa fa-user'></i> Sign In
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
 };
 
 export default Header;
